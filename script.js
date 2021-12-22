bd9c60254784143f96056e5c6290e7868c113ae1
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2021-12-22 15:04:03 +0600

    added index.html file

---
+++ script.js
@@ -0,0 +1,125 @@
+// // alert("hello");
+
+// // console.log("hello");
+
+// var firstname = "zaiyrbekov";
+// var lastname = "Syimyk";
+// var secondname = "sima";
+// var applename = "friut";
+// var icename = "isecrean";
+// var ballname = "football";
+// var instagramname = "syimyk_itsme";
+// var folowersname = "2027";
+// var tiktokname = "couple_smk";
+// var tallname = "Shamil";
+// var redname = "color";
+// var foxname = "animal";
+// var linename = "grass";
+// var roomname = "home";
+// var laptopname = "acer";
+// var phonename = "poco";
+// var eyename = "broWn";
+// var schoolname = "class";
+// var gmailname = "#";
+// var italyname = "#";
+// var brazilname = "#";
+// var openname = "#";
+// var javaname = "#";
+// var newname = "#";
+// var indexname = "#";
+// var stilename = "#";
+
+// // eaxmple 1 math
+// var potatoes = 476;
+// tomatoes = 84;
+// var vegetables = potatoes + tomatoes;
+// console.log(vegetables);
+// var result = vegetables / 2;
+// console.log(result);
+
+// // example 2 math
+
+// var result = (2 + 1) / 3 - 6 * 87;
+// console.log(result);
+
+// // example 3math
+// var a = 99999;
+// var b = 74644;
+// var rectangleArea = a * b;
+// console.log(rectangleArea);
+// // example 4math
+// var PI = 3.14;
+// var r = 10;
+// var circleArea = r * r * PI;
+// console.log(circleArea);
+// var a = 644;
+// var x = 22.998;
+// var b = 87.9;
+// var discriminahtArea = b * x - a * x;
+// console.log(discriminahtArea);
+
+// var a = 24;
+// var b = 76;
+// var rectangleArea = a * b;
+// console.log(rectangleArea);
+
+// var PI = 3.14;
+// var r = 18;
+// var circleArea = r * r * PI;
+// console.log(circleArea);
+
+// var PI = 3.14;
+// var r = 7;
+// var circleArea = 2 * PI * r;
+// console.log(circleArea);
+
+// var a = 59;
+// var b = 79;
+// var c = 84
+// var circleArea =a*a+b*b+c*c;
+// console.log(circleArea);
+
+
+var example1 = 10 === 10; //equals
+var example2 = 11 > 10;  //more than
+var example3 = 10 < 11;   //less than 
+var example4 = 10 >= 10;  //more than or equals
+var example5 = 10 <= 10;//less than or equals
+var example6 = 10 !== 10; // not equals
+
+
+
+
+var a = 5;
+var b = 10;
+var example7 = a < b;
+
+
+var c = 12;
+var d = 11;
+if  (c < d) {
+  alert("C is less than D");
+  console.log('hello world');
+}
+
+
+var e = 3.13;
+if (e !== 3.13){
+  alert("why dom't you know number PI?");
+}
+else{
+  alert("good job");
+}
+
+
+var age = 10;
+if (age >= 18) {
+  alert("you can vote!");
+
+
+}
+else{
+  alert("light is on")
+}
+
+var LightSwi3u
